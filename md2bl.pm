32ca0ed58208f6ac9c2064366bf7294639af8a82
newnakashima <newnakashima2014@gmail.com>
2019-03-27 18:20:31 +0900

    editorconfig導入

--- md2bl.pm
+++ md2bl.pm
@@ -4,63 +4,63 @@ use strict;
 use Exporter 'import';
 
 sub get_eol {
-	my $EOL = '\n';
-	if ($^O eq 'msys') {
-		$EOL = '\r\n';
-	}
-	return $EOL;
+    my $EOL = '\n';
+    if ($^O eq 'msys') {
+        $EOL = '\r\n';
+    }
+    return $EOL;
 }
 
 sub md2bl {
-	my $line = shift;
+    my $line = shift;
 
-	# インデント（半角スペース4個）を変換
-	$line = indent2minus($line);
+    # インデント（半角スペース4個）を変換
+    $line = indent2minus($line);
 
-	# 太字の記法を変換
-	$line = ast2sqt($line);
+    # 太字の記法を変換
+    $line = ast2sqt($line);
 
-	# 見出しの記法を変換
-	$line = hash2ast($line);
+    # 見出しの記法を変換
+    $line = hash2ast($line);
 
-	# 空白行を詰める
-	$line = delete_empty_line($line);
+    # 空白行を詰める
+    $line = delete_empty_line($line);
 
     # リンク記法を変換
     $line = link2link($line);
 
-	return $line;
+    return $line;
 }
 
 sub indent2minus {
-	my $input = shift;
-	$input =~ s/\s{4}/-/g;
-	return $input;
+    my $input = shift;
+    $input =~ s/\s{4}/-/g;
+    return $input;
 }
 
 sub ast2sqt {
-	my $input = shift;
-	$input =~ s/\*\*(.*)\*\*/''\1''/g;
-	return $input;
+    my $input = shift;
+    $input =~ s/\*\*(.*)\*\*/''\1''/g;
+    return $input;
 }
 
 sub hash2ast {
-	my $input = shift;
-	$input =~ /^(#+)\s/;
-	my $matched = $1;
-	my $asts = $matched;
-	$asts =~ s/#/*/g;
-	$input =~ s/${matched}/${asts}/;
-	return $input;
+    my $input = shift;
+    $input =~ /^(#+)\s/;
+    my $matched = $1;
+    my $asts = $matched;
+    $asts =~ s/#/*/g;
+    $input =~ s/${matched}/${asts}/;
+    return $input;
 }
 
 sub delete_empty_line {
-	my $input = shift;
-	my $EOL = get_eol();
-	if ($input !~ /^\s*${EOL}/) {
-		return $input;
-	} 
-	return;
+    my $input = shift;
+    my $EOL = get_eol();
+    if ($input !~ /^\s*${EOL}/) {
+        return $input;
+    } 
+    return;
 }
 
 sub link2link {
--- test.pl
+++ test.pl
@@ -3,7 +3,7 @@ use lib '.';
 use md2bl;
 
 sub exec_test {
-	my ($test_name, $inputs, $expected) = @_;
+    my ($test_name, $inputs, $expected) = @_;
     for (my $i = 0; $i < @$inputs; $i++) {
         my $actual = md2bl::md2bl($inputs->[$i]);
         if ($actual ne $expected->[$i]) {
@@ -17,81 +17,81 @@ sub exec_test {
 }
 
 sub test_indent2minus {
-	my @inputs = (
-		"- リストレベル1",
-		"    - リストレベル2",
-		"        - リストレベル3",
-	);
-	my @expected = (
-		"- リストレベル1",
-		"-- リストレベル2",
-		"--- リストレベル3",
-	);
+    my @inputs = (
+        "- リストレベル1",
+        "    - リストレベル2",
+        "        - リストレベル3",
+    );
+    my @expected = (
+        "- リストレベル1",
+        "-- リストレベル2",
+        "--- リストレベル3",
+    );
     exec_test((caller(0))[3], \@inputs, \@expected);
 }
 
 sub test_ast2sqt {
-	my @inputs = (
-		"**太字記法**",
-	);
-	my @expected = (
-		"''太字記法''",
-	);
+    my @inputs = (
+        "**太字記法**",
+    );
+    my @expected = (
+        "''太字記法''",
+    );
     exec_test((caller(0))[3], \@inputs, \@expected);
 }
 
 sub test_hash2ast {
-	my @inputs = (
-		"# 見出しレベル1",
-		"## 見出しレベル2",
-		"### 見出しレベル3",
-		"# 文の途中に#があるやつ",
-	);
-	my @expected = (
-		"* 見出しレベル1",
-		"** 見出しレベル2",
-		"*** 見出しレベル3",
-		"* 文の途中に#があるやつ",
-	);
+    my @inputs = (
+        "# 見出しレベル1",
+        "## 見出しレベル2",
+        "### 見出しレベル3",
+        "# 文の途中に#があるやつ",
+    );
+    my @expected = (
+        "* 見出しレベル1",
+        "** 見出しレベル2",
+        "*** 見出しレベル3",
+        "* 文の途中に#があるやつ",
+    );
     exec_test((caller(0))[3], \@inputs, \@expected);
 }
 
 sub test_delete_empty_line {
     my $test_name = (caller(0))[3];
-	my $inputs = [
-		["hoge\n", "\n", "hoge\n"],
-		["hoge\n", "  \n", "hoge\n"],
-	];
-	my $expected = [
-		["hoge\n", "hoge\n"],
-		["hoge\n", "hoge\n"],
-	];
-	for (my $i = 0; $i < scalar(@$inputs); $i++) {
-		my @actual = ();
-		foreach my $item (@{$inputs->[$i]}) {
+    my $inputs = [
+        ["hoge\n", "\n", "hoge\n"],
+        ["hoge\n", "  \n", "hoge\n"],
+    ];
+    my $expected = [
+        ["hoge\n", "hoge\n"],
+        ["hoge\n", "hoge\n"],
+    ];
+    for (my $i = 0; $i < scalar(@$inputs); $i++) {
+        my @actual = ();
+        foreach my $item (@{$inputs->[$i]}) {
             my $result = md2bl::md2bl($item);
             if ($result eq "") {
                 next;
             }
-			push(@actual, md2bl::md2bl($item));
-		}
-		for (my $j = 0; $j < scalar(@$expected); $j++) {
+            push(@actual, md2bl::md2bl($item));
+        }
+        for (my $j = 0; $j < scalar(@$expected); $j++) {
             if (@actual[$j] ne $expected->[$i]->[$j]) {
-				print("$test_name failed.\n");
-				print("actual: " . @actual[$j] . "\n");
-				print("expected: " . $expected->[$i]->[$j] . "\n");
-				exit(1);
-			}
-		}
-	}
-	print("$test_name succeeded!\n");
+                print("$test_name failed.\n");
+                print("actual: " . @actual[$j] . "\n");
+                print("expected: " . $expected->[$i]->[$j] . "\n");
+                exit(1);
+            }
+        }
+    }
+    print("$test_name succeeded!\n");
 }
 
 sub test_link2link {
-	my @inputs = (
-		"このバグについては、このページ https://backlog.com/ja/ が参考になります。",
+    my @inputs = (
+        "このバグについては、このページ https://backlog.com/ja/ が参考になります。",
         "このバグについては、このページ [Backlog](https://backlog.com/ja/) が参考になります。",
-	);
+    );
     my @expected = (
         "このバグについては、このページ https://backlog.com/ja/ が参考になります。",
         "このバグについては、このページ [[Backlog>https://backlog.com/ja/]] が参考になります。",
